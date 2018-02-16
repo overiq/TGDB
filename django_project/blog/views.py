c437ade121dae2f368ac1858c4c3154ead632e2e
OverIQ <infooveriq@gmail.com>
2018-02-16 20:46:31 +0530

    Lesson 10 (10a)

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
@@ -9,4 +10,9 @@ def index(request):
 
 def today_is(request):
     now = datetime.datetime.now()
-    return render(request, 'blog/datetime.html', {'now': now })
+    return render(request, 'blog/datetime.html', {
+                                    'now': now,
+                                    'template_name': 'blog/nav.html' ,
+                                    'base_dir': settings.BASE_DIR }
+                                )
+
