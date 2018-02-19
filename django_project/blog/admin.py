ed02e753d4e6af9efb72553d1ec836aed31c1425
OverIQ <infooveriq@gmail.com>
2018-02-19 16:06:30 +0530

    Lesson 16 (16a)

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_on')
+    search_fields = ['name', 'email']
+    ordering = ['-name']
+    list_filter = ['active']
+    date_hierarchy = 'created_on'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'author', 'category',)
+    search_fields = ['title', 'content']
+    ordering = ['-pub_date']
+    list_filter = ['pub_date']
+    date_hierarchy = 'pub_date'
+    # filter_horizontal = ('tags',)
+    raw_id_fields = ('tags',)
+    # prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('slug',)
+    fields = ('title', 'slug', 'content', 'author', 'category', 'tags',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name',)
+
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Tag, TagAdmin)
--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,23 +1,27 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True,  verbose_name="Author Name")
     email = models.EmailField(unique=True)
     active = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_logged_in = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name + " : " + self.email
+        return self.name + "-" + self.email
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(Author)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
@@ -34,7 +38,7 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, help_text="Slug will be generated automatically from the title of the post")
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author)
@@ -43,3 +47,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
