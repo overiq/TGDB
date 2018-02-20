c82b65e72c55539c818fd5beea5722c6c463690d
OverIQ <infooveriq@gmail.com>
2018-02-20 11:59:32 +0530

    Lesson 25 (25a)

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -151,3 +151,6 @@ MANAGERS = ADMINS
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^cadmin/', include('cadmin.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
