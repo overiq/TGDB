46170634a56cb2b0ccf6c9bdc41f4ec3fa153bab
OverIQ <infooveriq@gmail.com>
2018-02-20 19:18:20 +0530

    Lesson 31 (31a)

--- django_project/cadmin/urls.py
+++ django_project/cadmin/urls.py
@@ -3,6 +3,16 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^password-change-done/$',
+        auth_views.password_change_done,
+        {'template_name': 'cadmin/password_change_done.html'},
+        name='password_change_done'
+        ),
+    url(r'^password-change/$',
+        auth_views.password_change,
+        {'template_name': 'cadmin/password_change.html' , 'post_change_redirect': 'password_change_done'},
+        name='password_change'
+        ),
     url(r'^$', views.home, name='home'),
     url(r'^login/$', views.login, {'template_name': 'cadmin/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'cadmin/logout.html'}, name='logout'),
